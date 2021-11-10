c59a4dbeb49c6a290d87458f0e77c991190721fc
Power <Sarah.Power@dfo-mpo.gc.ca>
2021-11-09 16:38:27 -0800

    data read

--- aslw.R
+++ aslw.R
@@ -2,11 +2,14 @@
 
 ##libraries
 install.packages("librarian")
-librarian::shelf(dplyr, purrr, tidyr, reshape2)
+librarian::shelf(dplyr, purrr, tidyr, reshape2, openxlsx)
 
 ##input
-df1 <- read.csv("data/SOG_aslw_data1.csv")
-df2 <- read.csv("data/SOG_aslw_data2.csv")
-df3 <- read.csv("data/SOG_aslw_data3.csv")
+df1 <- read.xlsx(xlsxFile = "data/SOG_aslw_data1.xlsx", sheet = 1)
+df2 <- read.xlsx(xlsxFile = "data/SOG_aslw_data2.xlsx", sheet = 1)
+df3 <- read.xlsx(xlsxFile = "data/SOG_aslw_data3.xlsx", sheet = 1)
+#df1 <- read.xlsx("data/SOG_aslw_data1.xlsx")
+#df2 <- read.csv("data/SOG_aslw_data2.csv")
+#df3 <- read.csv("data/SOG_aslw_data3.csv")
 
 df <- rbind(df1, df2, df3)
