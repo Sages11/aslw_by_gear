4d5f08674937e657539e38de5e839494d35184ec
Power <Sarah.Power@dfo-mpo.gc.ca>
2021-11-09 19:46:06 -0800

    data manipulation plus initial graph

--- aslw.R
+++ aslw.R
@@ -2,14 +2,38 @@
 
 ##libraries
 install.packages("librarian")
-librarian::shelf(dplyr, purrr, tidyr, reshape2, openxlsx)
+librarian::shelf(dplyr, purrr, tidyr, reshape2, openxlsx, ggplot2, scales)
 
 ##input
 df1 <- read.xlsx(xlsxFile = "data/SOG_aslw_data1.xlsx", sheet = 1)
 df2 <- read.xlsx(xlsxFile = "data/SOG_aslw_data2.xlsx", sheet = 1)
 df3 <- read.xlsx(xlsxFile = "data/SOG_aslw_data3.xlsx", sheet = 1)
-#df1 <- read.xlsx("data/SOG_aslw_data1.xlsx")
-#df2 <- read.csv("data/SOG_aslw_data2.csv")
-#df3 <- read.csv("data/SOG_aslw_data3.csv")
 
-df <- rbind(df1, df2, df3)
+df <- rbind(df1, df2, df3) %>%
+  mutate(Year = as.factor(substr(Season,1,4)),
+         Gear = as.factor(Gear),
+         Age = as.factor(Year_of_Life),
+         Sex = as.factor(Sex),
+         Source = as.factor(Source)) %>%
+  select(-FishNum, -Year_of_Life) %>% #no need to know individual fish number
+  mutate(Source=recode(Source, 
+                         `6`="Fish & Bait",
+                         `5`="Test Fishery",
+                         `0`="Roe",
+                         `1`="Special Use"))%>%
+  filter(Source == "Test Fishery") %>% #take out test fishery
+  
+unique(df$Source)
+# Source codes are: 0 = roe, 5 = test, 6 = F&B, 1 - special use. Gear codes are 19 = gillnet, 29 = seine, 1 = other
+# Plot distribution of ages by gear and source, by year
+ageDistGearPlot <- ggplot(data = df , mapping = aes(x=Age, fill = Source)) +
+  scale_fill_viridis_b() +
+  geom_bar(position = "dodge") + 
+  labs(y = "Number of fish", fill = "Sample type") +
+  scale_x_continuous(breaks = pretty_breaks()) +
+  scale_y_continuous(labels = comma) +
+  facet_wrap(~ Year, nrow = 4, scales = "free_y") +
+  #myTheme + 
+  theme(legend.position = "top")
+ggsave(ageDistGearPlot, path = "figures")
+
