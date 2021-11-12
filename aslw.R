1cc5b0da3dff70e6cf264c266609056589a45788
Power <Sarah.Power@dfo-mpo.gc.ca>
2021-11-12 10:35:24 -0800

    Plots reporting sex difference, ratio and ratio data set

--- aslw.R
+++ aslw.R
@@ -4,6 +4,13 @@
 install.packages("librarian")
 librarian::shelf(dplyr, purrr, tidyr, reshape2, openxlsx, ggplot2, scales)
 
+##function
+`%!in%` <- Negate(`%in%`)
+
+##Settings for ggsave function
+mywidth <- 12
+myheight <- 7
+
 ##input
 df1 <- read.xlsx(xlsxFile = "data/SOG_aslw_data1.xlsx", sheet = 1)
 df2 <- read.xlsx(xlsxFile = "data/SOG_aslw_data2.xlsx", sheet = 1)
@@ -34,46 +41,62 @@ df <- df %>%
          #Gear = as.factor(Gear),
          Age = as.factor(Year_of_Life),
          Sex = as.factor(Sex),
-         Area = substr(Section, 1,2)) %>%
+         Area = as.factor(substr(Section, 1,2))) %>%
   select(-FishNum, -Year_of_Life) #no need to know individual fish number
   
 #These are the values we want to remove from df
-df4 <- df %>% filter((Sources == "Special Use area 13-14" && Area  %!in% c("13", "14")))
+df4 <- df %>% 
+  filter(Sources == "Special Use area 13-14") %>% 
+  filter(Area %!in% c("13", "14"))
 
-df5 <- df %>% filter(Sources == "Special Use area 13-14")
+df <- anti_join(df, df4) %>% 
+  filter(as.numeric(Age)< 9)
 
-unique(df5$Area)
-# Source codes are: 0 = roe, 5 = test, 6 = F&B, 1 - special use. Gear codes are 19 = gillnet, 29 = seine, 1 = other
+df5 <- df %>%  # This should have nothing in it if the anti_jion worked.
+  filter(Sources == "Special Use area 13-14") %>% 
+  filter(Area %!in% c("13", "14"))
 
+ratiodf <- df %>%
+  #filter(Sex != "U") %>%
+  group_by(Year, Sources, Sex) %>%
+  summarise(n = n()) %>%
+  pivot_wider(names_from = "Sex",
+              values_from = "n") %>%
+  mutate( n = F + M,
+    Fratio = F/n,
+    myaxis = paste0(Sources, "\n", "n=", n))
+
+unique(df$Sex)  
+##Plots
 
 ggplot(df, aes(x=Year, y=Length, fill = Sources)) + 
   #scale_fill_viridis_c() +
-  geom_boxplot() + 
+  geom_boxplot(position = position_dodge2(width = 0.9, preserve = "single")) + 
   labs(y = "Length (mm)", x = NULL)+
   theme(legend.position = "top",
         text = element_text(size = 20),
         legend.title = element_blank())  
   #facet_wrap(~Sex, col =1)
-ggsave("figures/length.png")
+ggsave("figures/length.png", width = mywidth, height = myheight)
 
 ggplot(df, aes(x=Year, y=Weight, fill = Sources)) + 
   #scale_fill_viridis_c() +
-  geom_boxplot() + 
+  geom_boxplot(position = position_dodge2(width = 0.9, preserve = "single")) + 
   labs(y = "Weight (g)")+
   theme(legend.position = "top",
         text = element_text(size = 20),
         legend.title = element_blank())  
-ggsave("figures/weight.png")
+ggsave("figures/weight.png", width = mywidth, height = myheight)
 
 ggplot(df, aes(x=Sex, y=Weight, fill = Sources)) + 
   #scale_fill_viridis_c() +
-  geom_boxplot() + 
+  geom_boxplot(position = position_dodge2(width = 0.9, preserve = "single")) + 
   #facet_wrap(~Age)+
   labs(y = "Weight (g)")+
   theme(legend.position = "top",
         text = element_text(size = 20),
         legend.title = element_blank())  
-ggsave("figures/sex.png")
+ggsave("figures/sex.png", width = mywidth, height = myheight)
 
 ggplot(data = df , mapping = aes(x=Age, fill = Sources)) +
   #scale_fill_viridis_b() +
@@ -85,7 +108,7 @@ ggplot(data = df , mapping = aes(x=Age, fill = Sources)) +
   theme(legend.position = "top",
         text = element_text(size = 18),
         legend.title = element_blank())  
-ggsave("figures/age.png")
+ggsave("figures/age.png", width = mywidth, height = myheight)
 
 ggplot(data = df , mapping = aes(x=Length, fill = Sources)) +
   #scale_fill_viridis_b() +
@@ -97,7 +120,7 @@ ggplot(data = df , mapping = aes(x=Length, fill = Sources)) +
   theme(legend.position = "top",
         text = element_text(size = 18),
         legend.title = element_blank())  
-ggsave("figures/lengthagegear.png")
+ggsave("figures/lengthagegear.png", width = mywidth, height = myheight)
 
 ggplot(data = df , mapping = aes(x=Length)) +
   #scale_fill_viridis_b() +
@@ -109,11 +132,11 @@ ggplot(data = df , mapping = aes(x=Length)) +
   theme(legend.position = "top",
         text = element_text(size = 16),
         legend.title = element_blank())  
-ggsave("figures/lengthage.png")
+ggsave("figures/lengthage.png", width = mywidth, height = myheight)
 
 ggplot(data = df , mapping = aes(x=Sex, fill = Sources)) +
   #scale_fill_viridis_b() +
-  geom_bar(position = "dodge") + 
+  geom_bar(position = position_dodge2(width = 0.9, preserve = "single")) + 
   labs(y = "Number of fish", fill = "Source") +
   #scale_x_continuous(breaks = pretty_breaks()) +
   scale_y_continuous(labels = comma) +
@@ -121,11 +144,23 @@ ggplot(data = df , mapping = aes(x=Sex, fill = Sources)) +
   theme(legend.position = "top",
         text = element_text(size = 16),
         legend.title = element_blank())  
-ggsave("figures/Sexage.png")
+ggsave("figures/Sexage.png", width = mywidth, height = myheight)
 
 ggplot(data = df , mapping = aes(x=Sex, fill = Sources)) +
   #scale_fill_viridis_b() +
-  geom_bar(position = "dodge") + 
+  geom_bar(position = position_dodge2(width = 0.9, preserve = "single")) + 
+  labs(y = "Number of fish", fill = "Source") +
+  #scale_x_continuous(breaks = pretty_breaks()) +
+  scale_y_continuous(labels = comma) +
+  facet_wrap(~ Year, ncol = 3, scales = "free_y", dir ="v") +
+  theme(legend.position = "top",
+        text = element_text(size = 16),
+        legend.title = element_blank())  
+ggsave("figures/Sexyear.png", width = mywidth, height = myheight)
+
+ggplot(data = df , mapping = aes(x=Sex, fill = Sources)) +
+  #scale_fill_viridis_b() +
+  geom_bar(position = position_dodge2(width = 0.9, preserve = "single")) + 
   labs(y = "Number of fish", fill = "Source") +
   #scale_x_continuous(breaks = pretty_breaks()) +
   scale_y_continuous(labels = comma) +
@@ -133,5 +168,18 @@ ggplot(data = df , mapping = aes(x=Sex, fill = Sources)) +
   theme(legend.position = "top",
         text = element_text(size = 16),
         legend.title = element_blank())  
-ggsave("figures/Sexgear.png")
 
+ggsave("figures/Sexgear.png", width = mywidth, height = myheight)
+
+ggplot(data = ratiodf , mapping = aes(x=Sources, y=F/(F+M), fill = Sources)) +
+  geom_bar(stat = "identity") + 
+  labs(y = "F/(F+M)", x = NULL) +
+  geom_hline(yintercept = 0.5, alpha = 0.7, linetype =2) +
+  geom_text(aes(label=n), position=position_dodge(width=0.9), vjust=-0.25)+
+  facet_wrap(~ Year, ncol = 3, scales = "free_y", dir ="h") +
+  theme(legend.position = "top",
+        text = element_text(size = 16),
+        axis.text.x=element_blank (),
+        legend.title = element_blank())  
+
+ggsave("figures/Sexgearratio.png", width = mywidth, height = myheight)
